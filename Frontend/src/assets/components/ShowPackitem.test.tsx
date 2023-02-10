b086810cb6f07f00cff26408a8da92e36ec04d60
Espen Matheussen <espen@antidata.no>
2023-02-10 10:58:22 +0100

    Wrote first successful tests using vitest

---
+++ Frontend/src/assets/components/ShowPackitem.test.tsx
@@ -0,0 +1,20 @@
+import { describe, expect, it, test } from "vitest";
+import ShowPackItem from "./ShowPackitem";
+import { queryAllByAttribute, queryByAttribute, queryByText, render, screen } from "@testing-library/react";
+import { shallowEqual } from "react-redux";
+
+describe("ShowPackItem component", () => {
+
+    // todo: test function handleCheckboxPackitem
+  it("shows one Packitem", () => {
+    const { container } = render(
+      <ShowPackItem title="Title" _id="12345" checked={false} />
+    );
+    expect(container).toBeInTheDocument();
+  });
+  it("checks the checkbox if checked", () => {
+    render(<ShowPackItem title="Title" _id="12345" checked={true} />);
+    expect(screen.getByText("Title")).toHaveClass('line-through');
+  });
+});
+
