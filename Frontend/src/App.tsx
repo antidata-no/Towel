d2f16039451bf804f3179d5423721c2ffc60f29e
antidata-no <espen@antidata.no>
2023-02-21 10:00:37 +0100

    refactor

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import {
   CategoryContext,
   CategoryDispatchContext,
@@ -11,28 +11,44 @@ import CreateCategory from "./assets/components/CreateCategory";
 import ListCategories from "./assets/components/ListCategories";
 import { apiGetCategories } from "./assets/api/apiGetCategories";
 import Frame from "./assets/components/UI/Frame";
+import ErrorModal from "./assets/components/UI/ErrorModal";
+import {
+  SetShowerrorModalContext,
+  ShowerrorModalContext,
+} from "./assets/contextreducer/ModalContext";
 
 function App() {
   let initialCategories: ICategory[] = [];
   const [categories, dispatch] = useReducer(categoryReducer, initialCategories);
+  const [showerror, setShowerror] = useState(false);
 
   useEffect(() => {
     async function fetchCategories() {
       initialCategories = await apiGetCategories();
-      await dispatch({type: "set", payload: initialCategories});
+      await dispatch({ type: "set", payload: initialCategories });
     }
     fetchCategories();
   }, []);
- 
+
   return (
     <div className="App">
       <Frame>
-      <CategoryContext.Provider value={categories}>
-        <CategoryDispatchContext.Provider value={dispatch}>
-          <CreateCategory />
-          <ListCategories />
-        </CategoryDispatchContext.Provider>
-      </CategoryContext.Provider>
+        <ErrorModal
+          title="Empty field"
+          message="Please enter a valid category name"
+          show={showerror}
+          setShow={setShowerror}
+        />
+        <ShowerrorModalContext.Provider value={showerror}>
+          <SetShowerrorModalContext.Provider value={setShowerror}>
+            <CategoryContext.Provider value={categories}>
+              <CategoryDispatchContext.Provider value={dispatch}>
+                <CreateCategory />
+                <ListCategories />
+              </CategoryDispatchContext.Provider>
+            </CategoryContext.Provider>
+          </SetShowerrorModalContext.Provider>
+        </ShowerrorModalContext.Provider>
       </Frame>
     </div>
   );
--- Frontend/src/assets/components/ListCategories.tsx
+++ Frontend/src/assets/components/ListCategories.tsx
@@ -5,34 +5,17 @@ import CreateItem from "./CreateItem";
 import ListItems from "./ListItems";
 import Collapse from "./UI/Collapse";
 
-import {
-  ShowerrorModalContext,
-  SetShowerrorModalContext,
-} from "../contextreducer/ModalContext";
-import ErrorModal from "./UI/ErrorModal";
-
 const ListCategories = () => {
   const categories = useContext(CategoryContext);
-  const [showerror, setShowerror] = useState(false);
 
   return (
     <>
-      <ErrorModal
-        title="Empty field"
-        message="Please enter a valid category name"
-        show={showerror}
-        setShow={setShowerror}
-      />
-      <ShowerrorModalContext.Provider value={showerror}>
-        <SetShowerrorModalContext.Provider value={setShowerror}>
-          {categories.map((category) => (
-            <Collapse title={category.title} key={category._id}>
-              <CreateItem category={category} />
-              <ListItems category={category} />
-            </Collapse>
-          ))}
-        </SetShowerrorModalContext.Provider>
-      </ShowerrorModalContext.Provider>
+      {categories.map((category) => (
+        <Collapse title={category.title} key={category._id}>
+          <CreateItem category={category} />
+          <ListItems category={category} />
+        </Collapse>
+      ))}
     </>
   );
 };
