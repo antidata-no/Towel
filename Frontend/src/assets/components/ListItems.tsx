9b08aced8f264fdeab50b9e1ce81514deee24695
antidata-no <espen@antidata.no>
2023-02-28 11:50:00 +0100

    changed delete button

--- Frontend/src/assets/components/ListItems.tsx
+++ Frontend/src/assets/components/ListItems.tsx
@@ -5,30 +5,29 @@ import { IPackitem, ICategory } from "../interfaces/Interfaces";
 
 const ListItems = ({ category }: { category: ICategory }) => {
 	return (
-		<>
-			<div className="overflow-x-auto">
-				<table className="table table-zebra w-full">
-					<tbody>
-						{category.items.map((packitem) => (
-							<tr key={packitem._id}>
-								<td>
-									<div className="flex flex-row">
-										<ShowPackItem
-											categoryid={category._id}
-											packitem={packitem}
-										/>
-										<DeletePackitemButton
-											category={category}
-											packitem={packitem}
-										/>
-									</div>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</>
+		<div className="overflow-x-auto">
+			<table className="table table-zebra w-full">
+				<tbody>
+					{category.items.map((packitem) => (
+						<tr key={packitem._id}>
+							<td>
+								<div className="flex flex-row items-center">
+									<ShowPackItem
+										categoryid={category._id}
+										packitem={packitem}
+									/>
+
+									<DeletePackitemButton
+										category={category}
+										packitem={packitem}
+									/>
+								</div>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 };
 
